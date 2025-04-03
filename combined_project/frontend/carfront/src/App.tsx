8ea52d62ce632042aa9f71e0a52eb5cb1e3a5e42
duawlsdn <wlsdn893@naver.com>
2025-04-03 21:32:28 +0900

    20250403 carlist get request

--- combined_project/frontend/carfront/src/App.tsx
+++ combined_project/frontend/carfront/src/App.tsx
@@ -3,6 +3,10 @@ import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import CssBaseline from "@mui/material/CssBaseline";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Carlist from "./components/Carlist";
+
+const queryClient = new QueryClient();
 
 function App() {
 
@@ -16,6 +20,9 @@ function App() {
           </Typography>
         </Toolbar>
       </AppBar>
+      <QueryClientProvider client={queryClient}>
+        <Carlist />
+      </QueryClientProvider>
     </Container>
   )
 }
