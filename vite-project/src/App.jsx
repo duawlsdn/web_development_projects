4e20b959ba7cf48a4f7f2f52fc057b396f5ee26e
duawlsdn <wlsdn893@naver.com>
2025-04-04 21:33:50 +0900

    feat: 20250404 delete function finished

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -4,19 +4,29 @@ import Result from './components/Result'
 import Start from './components/Start'
 import Title from './components/Title'
 import Values from './components/Inputs'
+import Modal from './components/Modal'
+import Copyright from './components/Copyright'
 
 function App() {
 
   return (
-    <section id='contents'>
-      <div className='wrapper'>
-        <Title />
-        <Intro />
-        <Values />
-        <Start />
-        <Result />
-      </div>
-    </section>
+    <body>
+      <section id='contents'>
+        <div className='wrapper'>
+          <Title />
+          <Intro />
+          <Values />
+          <Start />
+          <Result />
+        </div>
+      </section>
+      <section id='modal'>
+        <Modal />
+      </section>
+      <section id='copyright'>
+        <Copyright />
+      </section>
+    </body>
   )
 }
 
